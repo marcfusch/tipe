da7d9dc5e5033508589605c0e2e4f51175342f7b
marcfusch <marcfusch@gmail.com>
2023-02-19 18:24:30 +0100

    bruh

--- prusagen.sh
+++ prusagen.sh
@@ -2,20 +2,25 @@
 
 #install path
 slicerpath=/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer
-stl=./file.stl
-declare -a infills=("monotonic" "grid" "hiulbertcurve" "honeycomb" "octagramspiral" "stars" "triangle" "gyroid")
+stl=./cube.stl
+declare -a infills=("line" "cubic" "adaptivecubic" "rectilinear" "alignedrectilinear" "concentric" "grid" "hilbertcurve" "honeycomb" "3dhoneycomb" "archimedeanchords" "lightning" "octagramspiral" "stars" "gyroid" "triangles")
+
 
 for infill in ${infills[@]}; do
-    echo "Printing with infill pattern:" $infill
+    echo "Slicing with infill pattern:" $infill
 
     $slicerpath \
         --load ./config.ini \
         --printer-technology FFF \
-        --fill_pattern $infill \
-        --top_fill_pattern monotonic \
-        --bottom_fill_pattern monotonic \
+        --fill-pattern $infill \
+        --top-fill-pattern monotonic \
+        --bottom-fill-pattern monotonic \
         --slice \
-        --export-gcode \
-        $stl
-    sleep 5
+        -g \
+        $stl \
+        #>/dev/null 2&>1
+
+    
+    mv *.gcode output/${infill}.gcode
+    sleep 2
 done
